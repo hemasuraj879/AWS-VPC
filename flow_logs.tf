eb65359685f5cc72bf71cf99b9f9fc1a940e5c52
hemasuraj879 <your_email@example.com>
2024-12-20 12:13:31 +0530

    files updating

--- flow_logs.tf
+++ flow_logs.tf
@@ -7,6 +7,7 @@ resource "aws_flow_log" "vpc_flow_logs" {
 }
 
 resource "aws_cloudwatch_log_group" "vpx_flow_logs" {
+  count = var.is_flow_log_req ? 1 : 0
   name = "${var.project_name}-vpc_flow"
   skip_destroy = var.skip_destroy
   retention_in_days = var.retention_in_days
@@ -20,11 +21,13 @@ resource "aws_cloudwatch_log_group" "vpx_flow_logs" {
 }
 
 resource "aws_iam_role" "vpc_flow" {
+  count = var.is_flow_log_req ? 1 : 0
   name               = "${var.project_name}-vpc_flow"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_iam_role_policy" "vpc_flow" {
+  count = var.is_flow_log_req ? 1 : 0
   name   = "${var.project_name}-vpc_flow"
   role   = aws_iam_role.vpc_flow.id
   policy = data.aws_iam_policy_document.example.json
